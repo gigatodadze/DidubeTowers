b2e4195b4430e0cc9953b31716a4a8c0090b9a9a
gigatodadze <72299807+gigatodadze@users.noreply.github.com>
2021-01-27 22:43:06 +0400

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>დიდუბე თაუერსი</title>
+    <script src="/js/header.js"></script>
+    <link rel="stylesheet" href="/css/header.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="header">
+    <div class="sides">
+        <a href="#" class="logo">რემონტი</a>
+        <a href="#" class="logo">ყიდვა</a>
+        <a href="#" class="logo">ჩვენს შესახებ</a>
+
+    </div>
+    <div class="sides"> <a href="#" class="menu"> </a></div>
+    <div class="info">
+        <h1>შეიძინე გარემონტებული ბინა დიდუბე თაუერსში</h1>
+
+    </div>
+</div>
+<section class="content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisl turpis, porttitor et finibus id, viverra a metus. Praesent non ante sed orci posuere varius quis sit amet dui. Cras molestie magna orci, id gravida dolor molestie in. Duis sollicitudin turpis quis tortor egestas, ut ultrices nisl elementum. Vestibulum sed ipsum eget nulla laoreet cursus in ac sem. Integer a suscipit justo, quis aliquam sapien. Maecenas et tellus nibh. Vivamus tincidunt eros id commodo pellentesque.</p>
+    <p align="center"><a href="https://facebook.com/" class="btn facebook" target="_b">გამოგვყევით ფეისბუქზე</a>
+    </p>
+</section>
+
+
+
+</body>
+</html>
